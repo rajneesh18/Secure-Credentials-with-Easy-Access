4097c91dc1673547fd85b25d805106ef7b360785
Rajneesh Shukla <shukla.rajneesh2018@gmail.com>
2022-10-12 11:10:54 +0530

    update

--- frontend/src/routes/web.js
+++ frontend/src/routes/web.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import '../css/app.css';
 
 const baseURL = "https://localhost:3001/cred";
 
@@ -29,7 +30,7 @@ const App = () => {
         if(!active) {  alert('Click on Button Before'); return ; }
         if(password) {
             axios.post(baseURL, {
-
+                
             }).then((response) => {
                 
             });
@@ -42,35 +43,40 @@ const App = () => {
     }
 
     return <>
-        <div className="container">
-            <div>
-                <input value="Get" type="button" className={active === "get" ? "active" : undefined} onClick={(e) => setActive('get')} />&nbsp;
-                <input value="Add" type="button" className={active === "add" ? "active" : undefined} onClick={(e) => setActive('add')}  />&nbsp;
-                <input value="Update" type="button" className={active === "update" ? "active" : undefined} onClick={(e) => setActive('update')}  />&nbsp;
-                <input value="Delete" type="button" className={active === "delete" ? "active" : undefined} onClick={(e) => setActive('delete')}  />&nbsp;
+        <div className="secret-container">
+            <div className="secret-box">
+                <div>
+                    <h3 style={{fontFamily: "verdana", fontSize: "1rem", marginBottom: "30px", textAlign: "center"}}>Get Your Credentials</h3>
+                </div>
+                <div>
+                    <input className={`btn ${active === 'get' && 'active' }`} value="Get" type="button" onClick={(e) => setActive('get')} />&nbsp;
+                    <input className={`btn ${active === 'add' && 'active' }`} value="Add" type="button" onClick={(e) => setActive('add')}  />&nbsp;
+                    <input className={`btn ${active === 'update' && 'active' }`} value="Update" type="button" onClick={(e) => setActive('update')}  />&nbsp;
+                    <input className={`btn ${active === 'delete' && 'active' }`} value="Delete" type="button" onClick={(e) => setActive('delete')}  />&nbsp;
+                </div>
+                
+                <form id="cred-form" name="cred-form" onSubmit={handleSubmit}>
+                    <div>
+                        <label style={label}>Secret Key</label>:&nbsp;
+                        <input size="40" id="secret" name="secret" max="30" type="password" placeholder="Enter Secret Key" value={secret} onChange={(e) => setSecret(e.target.value) } />
+                    </div>
+                    <div>
+                        <label style={label}>Type</label>:&nbsp;
+                        <input size="40" id="type" name="type" max="30" placeholder="Enter Type" value={type} onChange={(e) => setType(e.target.value) } />
+                    </div>
+                    <div>
+                        <label style={label}>User</label>:&nbsp;
+                        <input size="40" id="user" name="user" max="40" placeholder="Enter User" value={user} onChange={(e) => setUser(e.target.value) } />
+                    </div>
+                    
+                    <div>
+                        <label style={label}>Password</label>:&nbsp;
+                        <input size="40" id="password" name="password" max="40" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value) } />
+                    </div>
+                    <br />
+                    <input className="btn" type="submit" id="submit-btn" value="Submit"  />
+                </form>
             </div>
-            <form id="cred-form" name="cred-form" onSubmit={handleSubmit}>
-                <div>
-                    <label style={label}>Secret Key</label>:&nbsp;
-                    <input size="40" id="secret" name="secret" max="30" type="password" placeholder="Enter Secret Key" value={secret} onChange={(e) => setSecret(e.target.value) } />
-                </div>
-                <div>
-                    <label style={label}>Type</label>:&nbsp;
-                    <input size="40" id="type" name="type" max="30" placeholder="Enter Type" value={type} onChange={(e) => setType(e.target.value) } />
-                </div>
-                <div>
-                    <label style={label}>User</label>:&nbsp;
-                    <input size="40" id="user" name="user" max="40" placeholder="Enter User" value={user} onChange={(e) => setUser(e.target.value) } />
-                </div>
-                <div>
-                    <label style={label}>Password</label>:&nbsp;
-                    <input size="40" id="password" name="password" max="40" placeholder="Enter Password" value={user} onChange={(e) => setPassword(e.target.value) } />
-                </div>
-                <br />
-                <input type="submit" id="submit-btn" value="Submit"  />
-            </form>
-
-            {secret}<br />{type}<br/>{user}
         </div>
     </>
 }
